c12b50c3ba513c3c8353ef35a2839b9396b95307
MezghaniDali <70319220+MezghaniDali@users.noreply.github.com>
2022-03-02 20:18:05 +0100

    added assets

--- Traffic_cars_v0.1-2D/Assets/Scripts/CarsMovement.cs
+++ Traffic_cars_v0.1-2D/Assets/Scripts/CarsMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarsMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
